c4b1d676fbbe9d2d2eef378ab525a2ff0ed2c17e
Lystrillian <webeugene@proton.me>
2024-07-30 15:02:17 +0300

    supports date, map, reexp, set

--- index.ts
+++ index.ts
@@ -1,22 +1,26 @@
 type Primitive = string | number | boolean | null | undefined;
-type DeepEqualValue = Primitive | object | any[];
+type DeepEqualValue =
+  | Primitive
+  | object
+  | any[]
+  | Date
+  | RegExp
+  | Map<any, any>
+  | Set<any>;
 
 export function deepEqual<T extends DeepEqualValue>(a: T, b: T): boolean {
   if (a === b) return true;
-
   if (a == null || b == null) return false;
-
   if (typeof a !== typeof b) return false;
 
   if (isPrimitive(a) || isPrimitive(b)) return a === b;
-
-  if (Array.isArray(a) && Array.isArray(b)) {
-    return arraysEqual(a, b);
-  }
-
-  if (typeof a === "object" && typeof b === "object") {
+  if (Array.isArray(a) && Array.isArray(b)) return arraysEqual(a, b);
+  if (a instanceof Date && b instanceof Date) return datesEqual(a, b);
+  if (a instanceof RegExp && b instanceof RegExp) return regexEqual(a, b);
+  if (a instanceof Map && b instanceof Map) return mapsEqual(a, b);
+  if (a instanceof Set && b instanceof Set) return setsEqual(a, b);
+  if (typeof a === "object" && typeof b === "object")
     return objectsEqual(a as object, b as object);
-  }
 
   return false;
 }
@@ -31,6 +35,33 @@ function isPrimitive(value: unknown): value is Primitive {
 function arraysEqual<T extends any[]>(a: T, b: T): boolean {
   if (a.length !== b.length) return false;
   return a.every((item, index) => deepEqual(item, b[index]));
+}
+
+function datesEqual(a: Date, b: Date): boolean {
+  return a.getTime() === b.getTime();
+}
+
+function regexEqual(a: RegExp, b: RegExp): boolean {
+  return a.source === b.source && a.flags === b.flags;
+}
+
+function mapsEqual<K, V extends DeepEqualValue>(
+  a: Map<K, V>,
+  b: Map<K, V>
+): boolean {
+  if (a.size !== b.size) return false;
+  for (const [key, value] of a) {
+    if (!b.has(key) || !deepEqual(value, b.get(key)!)) return false;
+  }
+  return true;
+}
+
+function setsEqual<T>(a: Set<T>, b: Set<T>): boolean {
+  if (a.size !== b.size) return false;
+  for (const item of a) {
+    if (!b.has(item)) return false;
+  }
+  return true;
 }
 
 function objectsEqual(a: object, b: object): boolean {
